a01e46423d63067a6ae9027e5af4aad4149bd4ae
Cratoinyan <umutciloglu06@gmail.com>
2022-10-11 16:13:31 +0300

    complete part 1 of the project

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 		log.Fatal(err)
 	}
 
+	score := 0
+
 	defer problemsCsv.Close()
 
 	csvReeader := csv.NewReader(problemsCsv)
@@ -26,4 +28,22 @@ func main() {
 	}
 
 	fmt.Println(reflect.TypeOf(data))
+
+	fmt.Println("-----Quiz is starting-----")
+
+	for i := 0; i < len(data); i++ {
+		fmt.Println(data[i][0], "is?")
+
+		var solution string
+		fmt.Scanf("%s", &solution)
+
+		if solution == data[i][1] {
+			fmt.Println("correct")
+			score++
+		} else {
+			fmt.Println("wrong")
+		}
+	}
+
+	fmt.Println("your score is ", score)
 }
