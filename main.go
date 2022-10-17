5889966ae2c12ec4b621217c1b17ea9bc737c8d5
Cratoinyan <umutciloglu06@gmail.com>
2022-10-17 16:19:10 +0300

    add comments to main.go

--- main.go
+++ main.go
@@ -10,28 +10,28 @@ import (
 )
 
 func main() {
-
+	//add flags for our cli
 	csvFileName := flag.String("csv", "problems.csv", "csv file with format 'question,answer'")
 	quizTime := flag.Int("time", 30, "time limit of the quiz")
-
 	flag.Parse()
 
+	//open csv file and control if it is succesfull
 	problemsCsv, err := os.Open(*csvFileName)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+	//init score
 	score := 0
 
+	//function to pass to time.AfterFunc so the quiz will be ended after the set amount given with the time flag has passed
 	finishQuiz := func() {
 		fmt.Println("your score is ", score)
 		os.Exit(0)
 	}
 
 	defer problemsCsv.Close()
-
 	csvReeader := csv.NewReader(problemsCsv)
-
 	data, err := csvReeader.ReadAll()
 
 	if err != nil {
@@ -39,9 +39,9 @@ func main() {
 	}
 
 	fmt.Println("-----Quiz is starting-----")
-
+	//set the timer
 	time.AfterFunc(time.Duration(*quizTime)*time.Second, finishQuiz)
-
+	//ask every question read from the quiz
 	for i := 0; i < len(data); i++ {
 		fmt.Println(data[i][0], "is?")
 
